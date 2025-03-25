55694c6c587c62362a6d58af8b86791c13423437
Ritu-dangi123 <dangiritu270@gmail.com>
2025-03-25 02:05:36 -0700

    bankproject

--- Project/src/Config.jsx
+++ Project/src/Config.jsx
@@ -1,3 +1,3 @@
-const BASE_URL = "https://metrobank.onrender.com";
-
+const BASE_URL = "https://bank-pcyn.onrender.com";
+// const BASE_URL = "https://metrobank.onrender.com";
 export default BASE_URL;
