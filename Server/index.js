276eedd11b5192e7e67fa81dbdb334eae1f363bc
Ritu-dangi123 <dangiritu270@gmail.com>
2025-03-25 01:16:06 -0700

    bankproject

--- Server/index.js
+++ Server/index.js
@@ -1,29 +1,24 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const cors = require("cors")
-const bodyparser = require("body-parser")
+const cors = require("cors");
+const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-const customerRoute = require("./Route/customerRoute")
+const customerRoute = require("./Route/customerRoute");
 require("dotenv").config();
 
-
 app.use(cors());
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-app.use("/uploads"  , express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
 
+mongoose.connect(process.env.DB_CONNECT).then(() => {
+  console.log("Database Connect");
+});
 
-mongoose.connect(process.env.DB_CONNECT)
-.then(()=>{
-    console.log("Database Connect")
-})
+app.use("/customer", customerRoute);
 
+const port = process.env.PORT || 8000;
 
-app.use("/customer",customerRoute)
-
-const port = process.env.PORT || 8000
-
-app.listen(port,()=>{
-    console.log("server on")
-})
-
+app.listen(port, () => {
+  console.log("server on");
+});
